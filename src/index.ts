aca0504a51cc52fa00193659e743a7403fb28dad
mahirdika <nabilpunyaakunbaru@gmail.com>
2026-03-08 23:46:49 +0700

    add body-parser and validation

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ dotenv.config();
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(cors());
 
 routes(app);
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import Joi from "joi";
+import { createProductValidation } from "../validation/product.validation.js";
 
 export const productRouter: Router = Router();
 
@@ -6,4 +8,13 @@ productRouter.get("/", (req, res) => {
   res
     .status(200)
     .send({ statuscode: 200, data: { name: "lemari", price: 1000000 } });
+});
+
+productRouter.post("/", (req, res) => {
+  const { error, value } = createProductValidation(req.body);
+  if (error) {
+    const message = error.details[0]?.message ?? "Validation error";
+    return res.status(400).send({ statuscode: 400, message: message });
+  } 
+  res.status(201).send({ statuscode: 201, data: req.body });
 });
